0f1a3dd333a1ec98e436a12ca38b00e4582af2a0
ntaifour <37583771+ntaifour@users.noreply.github.com>
2018-08-05 00:24:48 -0400

    added in table and dummy data

--- bamazon_schema.sql
+++ bamazon_schema.sql
@@ -13,3 +13,33 @@ CREATE TABLE products (
     PRIMARY KEY (item_id)
 );
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Sony WH-1000XM2', 'Electronics', 349.98, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Lamicall Cell Phone Stand', 'Cell Phones & Accessories', 9.99, 16);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('SUPCASE Unicorn Beetle', 'Cell Phones & Accessories', 6.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('YETI Rambler 20 oz', 'Home & Kitchen', 29.99, 31);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Energizer AA Alkaline Batteries (48-count)', 'Household Supplies)', 12.59, 300);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Sony PS4 Controller', 'Video Games', 46.96, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Cyxus Blue Light Blocking Computer Glasses', 'Health & Household', 19.90, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Samsung Gear S3 Frontier Smartwatch ', 'Electronics', 297.99, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Amazon Kindle Paperwhite E-reader', 'Electronics', 119.99, 65);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('New Balance 574 Sport Running Shoe', 'Clothing, Shoes & Jewelry ', 99.95, 2);
+
